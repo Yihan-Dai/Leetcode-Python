a2eee2f7b4900318d3320d85e498ed32f6874457
戴逸涵 <YihanDAI@daiyihandeMacBook-Pro.local>
2016-01-19 21:29:41 -0500

    TLE

---
+++ 4Sum/solutionTLE.py
@@ -0,0 +1,44 @@
+'''
+Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+Note:
+Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
+The solution set must not contain duplicate quadruplets.
+    For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
+
+    A solution set is:
+    (-1,  0, 0, 1)
+    (-2, -1, 1, 2)
+    (-2,  0, 0, 2)
+    
+'''
+
+
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        list =[]
+        if nums == list:
+            return list
+        nums = sorted(nums)
+        for i in range(len(nums)):
+            for k in range(i+1,len(nums)):
+                target1 = nums[i] + nums[k]
+                target2 = target -target1
+                m = k+1
+                n = len(nums) - 1
+                while m < len(nums): 
+                    if nums[m] + nums[n] == target2 and m != n:
+                        list.append([nums[i],nums[k],nums[m],nums[n]])
+                        m +=1
+                    else:
+                        n -=1
+                        if n+1 == m:
+                            m +=1
+                            n = len(nums) -1
+                            
+        return list
